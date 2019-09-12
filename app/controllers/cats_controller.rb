a5c9e2aa365730a4a6b1df46e4394ca63894bdd3
Charles <jslack2537@gmail.com>
2019-09-12 17:30:35 +0000

    added styling and functionality

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,4 +1,18 @@
 class CatsController < ApplicationController
   def index
+     @cat = Cat.all
   end
+
+  def create
+    Cat.create(cat_params)
+  
+    redirect_to root_path
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:compliment, :person)
+  end
+
 end
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,4 @@
 class Cat < ApplicationRecord
+  validates :person, presence: true, length: { maximum: 140, minimum: 3 }
+  validates :compliment, presence: true, length: { maximum: 50, minimum: 3 }
 end
