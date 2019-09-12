e5d7b6a3c4b626453f5288d9eb917f94e790f915
Charles <jslack2537@gmail.com>
2019-09-12 15:31:58 +0000

    added simple form gem

--- db/migrate/20190912152411_create_cats.rb
+++ db/migrate/20190912152411_create_cats.rb
@@ -1,7 +1,8 @@
 class CreateCats < ActiveRecord::Migration[5.2]
   def change
     create_table :cats do |t|
-
+      t.string :compliment
+      t.string :person
       t.timestamps
     end
   end
